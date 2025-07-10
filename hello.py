e50d696e356dcb2a498618619b68256e998348f4
musbiha-z <musbiha3469@gmail.com>
2025-07-10 19:01:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,4 @@ import sys
 print(sys.version)
 
 =======
-print ('hello, i am starting my google prep!')
->>>>>>> aa47877bce57abc6850b47041d1e7463f549b42e
+print ('hello, i am starting my google prep')
