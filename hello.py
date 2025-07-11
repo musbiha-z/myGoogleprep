dd94abd63125968cbcaf6efe7457875f33c6a07c
musbiha-z <musbiha3469@gmail.com>
2025-07-11 21:27:28 +0530

    fix: resolved conflict and updated hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+ # HEAD
 
 print('hello world!')
-=======
-<<<<<<< HEAD
+
+
 import sys
 print(sys.version)
 
-=======
+
 print ('hello, i am starting my google prep')
->>>>>>> e50d696e356dcb2a498618619b68256e998348f4
+
